b80abeafdd35f1c8ba429ff67217ba8913e223cc
Brad Chao <brad@brad.tw>
2016-09-29 16:21:56 +0800

    v6

--- app/src/main/java/brad/tw/myprivate/MainActivity.java
+++ app/src/main/java/brad/tw/myprivate/MainActivity.java
@@ -6,9 +6,11 @@ import android.accounts.AccountManager;
 import android.content.ContentResolver;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -18,15 +20,19 @@ import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     private TelephonyManager tmgr;
     private AccountManager amgr;
+    private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img = (ImageView)findViewById(R.id.img);
 
         // Android 6+
         if (Build.VERSION.SDK_INT >= 23) {
@@ -149,6 +155,22 @@ public class MainActivity extends AppCompatActivity {
             Log.d("brad", dname + ":" + dnum);
         }
 
+    }
+
+    public void test3(View view){
+        ContentResolver contentResolver = getContentResolver();
+        Cursor c = contentResolver.query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,null,null,null,null);
+
+        c.moveToNext();
+        c.moveToNext();
+        c.moveToNext();
+        c.moveToNext();
+        String photo = c.getString(c.getColumnIndex(MediaStore.Images.Media.DATA));
+        Log.d("brad", photo);
+
+        img.setImageBitmap(BitmapFactory.decodeFile(photo));
+//        img.setImageURI(Uri.parse(photo));
     }
 
 }
